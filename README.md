00000007a1d74ae1bbe3e910cbf38641a04a405b
graham jenson <graham.jenson@coinbase.com>
2018-01-16 11:59:57 +1300

    [Fix] now async and fast target:0000000 pow:56281932

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GitPOW
 
-GitPOW secures the history of your git repository with proof-of-work. Use GitPOW with `gitpow 000001a` it will alter your current git commit's message to find a SHA that starts with `000001a`. If you do this for every commit in your history, if someone wanted to change history they would need to redo all your work.
+GitPOW secures the history of your git repository with proof-of-work. Use GitPOW with `gitpow 000001a` it will alter your current git commit's message to find a SHA that starts with `000001a` (default `0000000`). This will protect against people rewriting history without putting all the work back in.
 
 # Build
 
--- gitpow.go
+++ gitpow.go
@@ -1,83 +1,131 @@
 package main
 
 import (
-  "fmt"
-  "crypto/sha1"
-  "encoding/hex"
-  "io"
-  "os/exec"
-  "time"
-  "strings"
-  "os"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
 )
 
 func main() {
-  target := "0000000"
-    if(len(os.Args) > 1) {
-      target = os.Args[1]
-    }
-    message := gitpow(target)
-    fmt.Printf("new message: %v\n", message)
+	target := "0000000"
+	if len(os.Args) > 1 {
+		target = os.Args[1]
+	}
+	message := gitpow(target)
+	fmt.Printf("new message: %v\n", message)
 }
 
-func gitpow(target string) string {
-  date := time.Now().Format(time.RFC3339)
-  git_init(date)
-  commit := git_commit()
-  old_message := git_message(commit)
-  m := 0
+func proover(id int, target string, date string, commit string, queries chan string, result chan string) {
+	// recieve message to process
+	// process message
+	fmt.Printf("WORKER %v started\n", id)
+	drain := false
+	l := len(target)
+	pre_old := commit[0 : strings.Index(commit, "\n\n")+2]
 
-  for(true) {
-    new_message := fmt.Sprintf("%v target:%v pow:%v", old_message, target, m)
-    new_commit := strings.Replace(commit, old_message, new_message, 1)
-    sha := git_sha(new_commit)
-    if(m % 100000 == 0) {
-      fmt.Printf("nm:%q sha:%q\n", new_message, sha)
-    }
+	for {
+		new_message, more := <-queries
+		if !more || drain {
+			fmt.Printf("WORKER %v finished\n", id)
+			return
+		}
 
-    if(strings.HasPrefix(sha, target)) {
-      git_update(date, new_message)
-      return new_message
-    }
-    m += 1
-  }
-  return "nope"
+		new_commit := fmt.Sprintf("%v%v\n", pre_old, new_message)
+		sha := git_sha(new_commit)
+
+		if target == sha[0:l] {
+			git_update(date, new_message)
+			fmt.Printf("WORKER %v pushing to result\n", id)
+			result <- new_message
+			// drain the queue stop blocking on input
+			drain = true
+		}
+	}
+}
+
+func gitpow(target string) (bool string) {
+	date := time.Now().Format(time.RFC3339)
+	git_init(date)
+	commit := git_commit()
+	old_message := git_message(commit)
+	workers := runtime.NumCPU() - 1
+
+	if workers <= 0 {
+		workers = 1
+	}
+
+	// Channel for numbers to be processed buffer of 5 per worker
+	queries := make(chan string, workers*5)
+
+	// results for processed numbers (requires size of workers in case of both finding an answer)
+	result := make(chan string, workers)
+
+	// Nu,ber of workers
+
+	for w := 1; w <= workers; w++ {
+		go proover(w, target, date, commit, queries, result)
+	}
+
+	m := 0
+	for {
+		select {
+		case res := <-result:
+			// closing the queue will set more to false
+			close(queries)
+			close(result)
+			return res
+		default:
+			new_message := fmt.Sprintf("%v target:%v pow:%v", old_message, target, m)
+			if m%1000000 == 0 {
+				fmt.Printf("nm:%q\n", new_message)
+			}
+			queries <- new_message
+			m += 1
+		}
+	}
+	fmt.Printf("RETURNING NOTHING\n")
+	return "nope"
 }
 
 func git_update(date string, new_message string) {
-  date_exp := fmt.Sprintf("GIT_COMMITTER_DATE=%v", date)
-  cmd := exec.Command("git", "commit", "--amend", "-m", new_message)
-  cmd.Env = append(os.Environ(), date_exp)
-  cmd.Run()
+	date_exp := fmt.Sprintf("GIT_COMMITTER_DATE=%v", date)
+	cmd := exec.Command("git", "commit", "--amend", "-m", new_message)
+	cmd.Env = append(os.Environ(), date_exp)
+	cmd.Run()
 }
 
 func git_init(date string) {
-  date_exp := fmt.Sprintf("GIT_COMMITTER_DATE=%v", date)
-  date_arg := fmt.Sprintf("--date=%v", date)
+	date_exp := fmt.Sprintf("GIT_COMMITTER_DATE=%v", date)
+	date_arg := fmt.Sprintf("--date=%v", date)
 
-  cmd := exec.Command("git", "commit", "--amend", "--no-edit", date_arg)
-  cmd.Env = append(os.Environ(), date_exp)
-  cmd.Run()
+	cmd := exec.Command("git", "commit", "--amend", "--no-edit", date_arg)
+	cmd.Env = append(os.Environ(), date_exp)
+	cmd.Run()
 }
 
 func git_commit() string {
-  cmd := exec.Command("git", "cat-file", "commit", "HEAD")
-  out, _ := cmd.Output()
-  return string(out)
+	cmd := exec.Command("git", "cat-file", "commit", "HEAD")
+	out, _ := cmd.Output()
+	return string(out)
 }
 
 func git_message(commit string) string {
-  return commit[strings.Index(commit, "\n\n")+2:len(commit)-1]
+	return commit[strings.Index(commit, "\n\n")+2 : len(commit)-1]
 }
 
 func git_sha(commit string) string {
-  return sha1sum(fmt.Sprintf("commit %v\u0000%v", len(commit), commit))
+	return sha1sum(fmt.Sprintf("commit %v\u0000%v", len(commit), commit))
 }
 
 func sha1sum(s string) string {
-  hasher := sha1.New()
-  io.WriteString(hasher, s)
-  sha := hex.EncodeToString(hasher.Sum(nil))
-  return sha
+	hasher := sha1.New()
+	io.WriteString(hasher, s)
+	sha := hex.EncodeToString(hasher.Sum(nil))
+	return sha
 }
-
